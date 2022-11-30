a19824bb36345443fb149adadc2c6a5e2ff203ab
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-11-30 15:15:53 -0700

    implement create job posts endpoint

--- Backend/src/routes/job-posts/index.js
+++ Backend/src/routes/job-posts/index.js
@@ -1,40 +1,41 @@
 const { Router } = require("express");
 const router = Router();
 const Database = require("better-sqlite3");
+const {uuid} = require('../../utils/GenerateID');
+const {getCurrentDate} = require('../../utils/Date');
 const db = new Database("freelance.db", { verbose: console.log });
 
-
 /**
- * Handle get request, queries the database to get the profile details of the 
+ * Handle get request, queries the database to get the profile details of the
  * provided user
  * @param role - from req.params
  * @param id - from req.params
- * @returns {object} - returns user details if user exists, 
+ * @returns {object} - returns user details if user exists,
  * else returns a 400 response status
  */
- router.get("/:role/:id", async (req, res) => {
+router.get("/:role/:id", async (req, res) => {
   try {
-	  const { role } = req.params;
-	  const { id } = req.params;
-	  let queryResult;
-	
-	  if (role === "freelancer") {
-	    //queryResult = queryFreelancer(id);
-	  } else if ((role === "business")){
-	    queryResult = queryHiringManager(id);
-	  } else {
+    const { role } = req.params;
+    const { id } = req.params;
+    let queryResult;
+
+    if (role === "freelancer") {
+      //queryResult = queryFreelancer(id);
+    } else if (role === "business") {
+      queryResult = queryHiringManager(id);
+    } else {
       return res.status(404).json({ error: "incorrect role was provided" });
     }
-	  return res.status(200).json({results: queryResult});
-} catch (error) {
-  console.log(error);
-	return res.status(500).json({ error: "Server Error" });
-}
+    return res.status(200).json({ results: queryResult });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server Error" });
+  }
 });
 
-router.get("/:jobID", async(req, res) => {
+router.get("/:jobID", async (req, res) => {
   try {
-	  const { jobID } = req.params;
+    const { jobID } = req.params;
     let sql = `
     SELECT f.FirstName || ' ' || f.LastName AS name, 
     f.Email AS email, a.YearsOfExperience, f.Location AS location,
@@ -42,26 +43,118 @@ router.get("/:jobID", async(req, res) => {
     FROM application AS a, freelancer AS f
     WHERE JID=?
     AND FID=f.ID
-    `
+    `;
 
     let stmt = db.prepare(sql);
     const queryResult = stmt.all(jobID);
-	
-	  return res.status(200).json({results: queryResult});
-} catch (error) {
-  console.log(error);
-	return res.status(500).json({ error: "Server Error" });
-}
+
+    return res.status(200).json({ results: queryResult });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server Error" });
+  }
 });
 
 /**
- * perform sql query to get the profile details of the freelancer
+ * Handle login post request, validate login credentials
+ * @param username - from req.body
+ * @param password - from req.body
+ * @returns {object} - returns user if user exists, else returns a 400 response status
+ */
+router.post("/create", async (req, res) => {
+  try {
+    const { HID } = req.body; //hiring manager id
+    let BID; //bussiness id
+    const { jobName, duration, locations, salary, skills, description , workingHours} =
+      req.body;
+    let industry;
+    const jobPostID = uuid();
+    const date = getCurrentDate();
+
+    //query to get the id and industry of the business
+    sql = `
+      SELECT worksFor AS BID, Industry AS industry
+      FROM hiring_manager AS h, business AS b
+      WHERE b.ID=worksFor
+      AND h.ID=?
+    `;
+
+    try {
+      let stmt = db.prepare(sql);
+      const queryResult = stmt.all(HID)[0];
+      BID = queryResult.BID;
+      industry = queryResult.industry;
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error: "hiring manager not found" });
+    }
+
+    //query to insert job post into the job_posts table 
+    sql = `
+      INSERT INTO job_post
+      VALUES (?,?,?,?,?,?,?,?,?)
+    `;
+    stmt = db.prepare(sql);
+    stmt.run(duration, workingHours, salary, description, jobName, jobPostID, date, industry, BID);
+
+    skills.map((skill) => {
+      skill = skill.charAt(0).toUpperCase() + skill.slice(1);
+      try {
+        sql = `
+        SELECT *
+        FROM skills
+        WHERE skillName=?
+        `
+        stmt = db.prepare(sql);
+        const result = stmt.all(skill);
+        if (result.length === 0) {
+          const skillID = uuid();
+          sql = `
+            INSERT INTO skills
+            VALUES (?,?)
+          `
+          stmt = db.prepare(sql);
+          const result = stmt.run(skillID,skill);
+          console.log(result);
+          sql = `
+          INSERT INTO requires
+          VALUES (?,?)
+          `
+          stmt = db.prepare(sql);
+          stmt.run(skillID,jobPostID);
+        } else {
+          const skillID = result[0].ID;
+          sql = `
+          INSERT INTO requires
+          VALUES (?,?)
+          `
+          stmt = db.prepare(sql);
+          stmt.run(skillID,jobPostID);
+        }
+
+        
+      } catch (error) {
+        console.log(error);
+      }
+    })
+
+    
+    
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Server Error" });
+  }
+});
+
+/**
+ * perform sql query to get job posts appropriate to the user
  * @param {string} id - id of freelancer
  * @returns {object} -returns result of sql query
  */
 //  function queryFreelancer(id) {
 //   let sql = null; // for sql statements
-  
+
 //   //sql query
 //   sql = `
 //     SELECT *
@@ -75,14 +168,14 @@ router.get("/:jobID", async(req, res) => {
 // }
 
 /**
- * perform sql query to get the profile details of the hiring manager and 
+ * perform sql query to get the profile details of the hiring manager and
  * the company details, the hiring manager works for
  * @param {string} id - id of hiring manager
  * @returns {object} - return result of sql query
  */
- function queryHiringManager(id) {
+function queryHiringManager(id) {
   let sql; // for sql statements
-  
+
   //sql query
   sql = `
     SELECT Duration, WorkingHours, 
@@ -97,6 +190,5 @@ router.get("/:jobID", async(req, res) => {
   const result = stmt.all(id);
   return result;
 }
-
 
 module.exports = { jobPostsRouter: router };
